839a150497db8ce88f9963ba410f0e14e816000f
jwlee <lemonade4813@gmail.com>
2024-04-14 23:59:06 +0900

    feat : 호텔 리스트 조회 amadeus API 연동

--- src/app/hotel/page.tsx
+++ src/app/hotel/page.tsx
@@ -1,7 +1,26 @@
+"use client"
+
+import { useFetch } from "../util/useFetch"
+
 export default function Hotel(){
+
+
+    const hotelListCallUrl = `https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=PAR&radius=5`
+
+
+    const {data , isLoading, error} = useFetch(hotelListCallUrl);
+
+    const hotelList = data?.data;
+
     return(
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <h2>호텔 예약</h2>
+            {hotelList?.map((hotel : any)=> (
+                <div>
+                    <p>{hotel.name}</p>
+                    <p>{hotel.lastUpdate}</p>
+                </div>
+            ))}
         </div>
     )
 }
