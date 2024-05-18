0d0baa15bc7d1dac6dcf18c6e012988f8f6ec5b4
jwlee <lemonade4813@gmail.com>
2024-05-19 00:37:57 +0900

    fix : 로그인 페이지 유효성 체크 적용(register 함수 사용)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 export default function Login(){
 
 
-        const {register, handleSubmit, watch, formState : {isSubmitting, isValid, errors}} = useForm<any>({mode : "onChange"});
+        const {register, handleSubmit, watch, formState : {isValid, errors}} = useForm<any>({mode : "onSubmit"});
 
 
         const onSubmit = (data : any) => {
@@ -17,17 +17,19 @@ export default function Login(){
                 <div className="absolute w-[400px] h-[120px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <h1 className="text-[40px] mb-8">로그인</h1>
                         <form className="flex flex-col items-center justify-center"
-                        onSubmit={onSubmit}   
+                        onSubmit={handleSubmit(onSubmit)}   
                         >
                         <div className="mb-8 group"> 
                                 <label htmlFor="id" className="mr-4 w-16 inline-block">아이디</label>
-                                <input className="border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg" id="id"/>
+                                <input {...register("id", { required: '아이디를 입력해주세요.'})} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
+                                <p className="text-[#DC143C] text-sm mt-2">{errors?.id?.message?.toString()}</p>
                         </div>
                         <div className="group">
                                 <label htmlFor="password" className="mr-4 w-16 inline-block">패스워드</label>
-                                <input className="border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg" type="password" id="password"/>     
-                        </div>    
-                        <button className={`h-12 w-24 h-10 text-white rounded-lg mt-8`}>로그인</button>
+                                <input {...register("password", { required: '패스워드를 입력해주세요.' })} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg" type="password" id="password"/>     
+                                <p className="text-[#DC143C] text-sm mt-2">{errors?.password?.message?.toString()}</p>
+                        </div>
+                        <button className={`${isValid ? 'bg-[#B80000]' :'bg-gray-300'} h-12 w-24 h-10 text-white rounded-lg mt-8`}>로그인</button>
                         </form>
                 </div>
         )
