e32dafa3de9f22ae5111bd918def29893d0b2fcb
jwlee <lemonade4813@gmail.com>
2024-08-30 00:58:15 +0900

    fix : 회원가입, 로그인 페이지 UI 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,15 +40,15 @@ export default function Login(){
                         >
                         <div className="mb-8 group"> 
                                 <label htmlFor="id" className="mr-4 w-16 inline-block">아이디</label>
-                                <input {...register("userId", { required: '아이디를 입력해주세요.'})} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
+                                <input {...register("userId", { required: '아이디를 입력해주세요.'})} className="shadow-sm bg-pink-100 w-[240px] h-12 rounded-lg"/>
                                 <p className="text-[#DC143C] text-sm mt-2">{errors?.id?.message?.toString()}</p>
                         </div>
                         <div className="group">
                                 <label htmlFor="password" className="mr-4 w-16 inline-block">패스워드</label>
-                                <input {...register("password", { required: '패스워드를 입력해주세요.' })} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg" type="password" id="password"/>     
+                                <input {...register("password", { required: '패스워드를 입력해주세요.' })} className="shadow-sm bg-pink-100 w-[240px] h-12 rounded-lg" type="password" id="password"/>     
                                 <p className="text-[#DC143C] text-sm mt-2">{errors?.password?.message?.toString()}</p>
                         </div>
-                        <button className={`${isValid ? 'bg-[#B80000]' :'bg-gray-300'} h-12 w-24 h-10 text-white rounded-lg mt-8`}>로그인</button>
+                        <button className={`${isValid ? 'bg-[#B80000]' :'bg-gray-300'} w-40 h-12 text-white rounded-lg mt-8`}>로그인</button>
                         </form>
                 </div>
         )
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,17 +4,12 @@ import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 
 export default function Signup(){
-
-
     const router = useRouter();
 
     const {register, watch, handleSubmit, formState : {errors, isValid}} = useForm<any>({mode : "onChange"})
 
-
     const submitSignupInfo = (data : any) => {
-        
         console.log(data)
-
 
         fetch('http://localhost:8080/signup', 
                 {
@@ -34,42 +29,45 @@ export default function Signup(){
     const password = watch('password');
 
     return(
-        <div className="absolute w-[400px] h-[120px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        <h1 className="text-[40px] mb-8">회원가입</h1>
-                        <form  onSubmit={handleSubmit(submitSignupInfo)}
-                                className="flex flex-col items-center justify-center">
-                            <div className="mb-8 group"> 
-                                    <label htmlFor="userId" className="mr-4 w-24 inline-block">아이디</label>
-                                    <input {...register("userId", { required: '아이디를 입력해주세요.'})} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
-                                    <p className="text-[#DC143C] text-sm mt-2">{errors?.id?.message?.toString()}</p>
-                            </div>
-                            <div className="mb-8 group"> 
-                                    <label htmlFor="password" className="mr-4 w-24 inline-block">비밀번호</label>
-                                    <input 
-                                        type="password"
-                                    {
-                                        ...register("password", {pattern : {value : /^(?=.*[A-Za-z])(?=.*[0-9]|.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{10,}$|^(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{10,}$/, message : '영문자, 숫자, 특수문자 중 2종류 이상을 포함하고 최소 10자 이상이어야 합니다.'}})}className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
-                                    <p className="text-[#DC143C] text-sm mt-2">{errors?.password?.message?.toString()}</p>
-                            </div>
-                            <div className="mb-8 group">
-                                    <label htmlFor="passwordConfirm" className="mr-4 w-24 inline-block">비밀번호 확인</label>
-                                    <input 
-                                        type="password"
-                                    {...register("passwordConfirm", { 
-                                        validate: value => value === password || '비밀번호가 일치하지 않습니다.' 
-                                    })} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
-                                    <p className="text-[#DC143C] text-sm mt-2">{errors?.passwordConfirm?.message?.toString()}</p>
-                            </div>
-                            <div className="mb-8 group"> 
-                                    <label htmlFor="phone" className="mr-4 w-24 inline-block">전화번호</label>
-                                    <input {...register("phone", { pattern : {value : /^\d+$/, message : '숫자만 입력 가능합니다.'}})} className="shadow-lg border-solid border-pink-400 border-2 w-[240px] h-12 rounded-lg"/>
-                                    <p className="text-[#DC143C] text-sm mt-2">{errors?.phone?.message?.toString()}</p>
-                            </div>
-                            <button
-                                type="submit" 
-                                className={`rounded-lg w-40 h-12 mt-20  text-white ${isValid ? 'bg-red-800' : 'bg-gray-400'}`} disabled={isValid ? false : true}>제출
-                            </button>
-                        </form>
-                </div>
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-[400px]">
+                <h1 className="text-[40px] mb-8">회원가입</h1>
+                <form onSubmit={handleSubmit(submitSignupInfo)} className="flex flex-col items-center justify-center">
+                    <div className="mb-8 group"> 
+                        <label htmlFor="userId" className="mr-4 w-24 inline-block">아이디</label>
+                        <input {...register("userId", { required: '아이디를 입력해주세요.'})} className="shadow-sm bg-pink-100 w-[240px] h-12 rounded-lg"/>
+                        <p className="text-[#DC143C] text-sm mt-2 max-w-[300px]">{errors?.id?.message?.toString()}</p>
+                    </div>
+                    <div className="mb-8 group"> 
+                        <label htmlFor="password" className="mr-4 w-24 inline-block">비밀번호</label>
+                        <input 
+                            type="password"
+                            {...register("password", {pattern : {value : /^(?=.*[A-Za-z])(?=.*[0-9]|.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{10,}$|^(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{10,}$/, message : '영문자, 숫자, 특수문자 중 2종류 이상을 포함하고 최소 10자 이상이어야 합니다.'}})} 
+                            className="shadow-sm bg-pink-100 w-[240px] h-12 rounded-lg"/>
+                        <p className="text-[#DC143C] text-sm mt-2 max-w-[300px]">{errors?.password?.message?.toString()}</p>
+                    </div>
+                    <div className="mb-8 group">
+                        <label htmlFor="passwordConfirm" className="mr-4 w-24 inline-block">비밀번호 확인</label>
+                        <input 
+                            type="password"
+                            {...register("passwordConfirm", { 
+                                validate: value => value === password || '비밀번호가 일치하지 않습니다.' 
+                            })} 
+                            className="shadow-sm bg-pink-100 w-[240px] h-12 rounded-lg"/>
+                        <p className="text-[#DC143C] text-sm mt-2 max-w-[300px]">{errors?.passwordConfirm?.message?.toString()}</p>
+                    </div>
+                    <div className="mb-8 group"> 
+                        <label htmlFor="phone" className="mr-4 w-24 inline-block">전화번호</label>
+                        <input {...register("phone", { pattern : {value : /^\d+$/, message : '숫자만 입력 가능합니다.'}})} className="shadow-sm border-black bg-pink-100 w-[240px] h-12 rounded-lg"/>
+                        <p className="text-[#DC143C] text-sm mt-2 max-w-[300px]">{errors?.phone?.message?.toString()}</p>
+                    </div>
+                    <button
+                        type="submit" 
+                        className={`rounded-lg w-40 h-12 mt-20 text-white ${isValid ? 'bg-red-800' : 'bg-gray-400'}`} 
+                        disabled={!isValid}>제출
+                    </button>
+                </form>
+            </div>
+        </div>            
     )
 }
