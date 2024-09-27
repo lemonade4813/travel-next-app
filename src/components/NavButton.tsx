6766202b95c649e34f6b18cca052575b8b6e04c1
jwlee <lemonade4813@gmail.com>
2024-09-27 10:13:25 +0900

    fix : 환경 변수 수정 등.

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -8,7 +8,7 @@ export default function NavButton({to} : {to : string}){
 
     return(
         <button 
-            className={'rounded-lg w-40 h-12 mt-20 bg-red-800 text-white'}
+            className={'rounded-lg w-40 h-12 bg-red-800 text-white'}
             onClick={() => router.push(to)}
         >조회하기
         </button>
